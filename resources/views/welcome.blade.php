cb556e0f55c4314735b3af036f6d91ab7c3b8abb
jgcoder <jjgu.tec@gmail.com>
2022-10-29 17:47:48 -0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JGcoder-Blog</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+
+  <body>
+    <div class="flex items-center justify-center h-screen bg-gradient-to-b from-indigo-400 ...">
+      <div class="flex flex-col items-center justify-center max-w-lg">
+        <div class="mb-4">
+          <h1 class="text-5xl font-extrabold text-blue-600">JGcoder</h1>
+        </div>
+        <h3 class="mb-3 text-2xl font-bold text-center text-gray-700">
+            Temporalmente en mantenimiento ¡Pronto el nuevo Blog!
+        </h3>
+        <p class="text-sm text-center text-gray-600">
+           
+        </p>
+      </div>
+    </div>
+  </body>
+
+</html>
